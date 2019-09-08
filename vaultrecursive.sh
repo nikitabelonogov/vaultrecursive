8853dc0dd7f410edea691aca6300384249bfe49a
Nikita Belonogov <belonogovna@sgu.ru>
2019-09-08 20:08:30 +0400

    Add logging and coloring

--- vaultrecursive.sh
+++ vaultrecursive.sh
@@ -2,6 +2,10 @@
 
 set -ue ${DEBUG:+-x}
 
+FC=$'\033[30m'
+SC=$'\033[33m'
+NC=$'\033[0m'
+
 [ -x "$(command -v jq)" ]
 [ -x "$(command -v vault)" ]
 
@@ -10,6 +14,7 @@ recursive_walk() {
   local path="${2}"
   for item in $(vault list -format=json "${path}" | jq -r '.[]'); do
     if [ "${item%/}" == "${item}" ]; then
+      echo "${FC}${command}${NC} ${SC}${path}/${item}${NC}"
       vault "${command}" "${path}/${item}"
     else
       recursive_walk "${command}" "${path}/${item%/}"
